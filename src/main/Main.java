c8951c3edc81d8867e56ade1d9bd70f61cd63eaa
jim-low <robil12380@gmail.com>
2021-07-30 20:03:23 +0800

    feat: add Infinity Academy welcome banner

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,16 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Education System for Infinity Academy!");
+        showAcademyBanner();
+    }
+
+    public static void showAcademyBanner() {
+        // whoever messes up the formatting and/or alignment of this here banner, will, by default, accept the consequences to come
+        System.out.println("__      __   _                    _         ___       __ _      _ _            _              _");
+        System.out.println("\\ \\    / /__| |__ ___ _ __  ___  | |_ ___  |_ _|_ _  / _(_)_ _ (_) |_ _  _    /_\\  __ __ _ __| |___ _ __ _  _");
+        System.out.println(" \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_) |  _/ _ \\  | || ' \\|  _| | ' \\| |  _| || |  / _ \\/ _/ _` / _` / -_) '  \\ || |");
+        System.out.println("  \\_/\\_/\\___|_\\__\\___/_|_|_\\___|  \\__\\___/ |___|_||_|_| |_|_||_|_|\\__|\\_, | /_/ \\_\\__\\__,_\\__,_\\___|_|_|_\\_, |");
+        System.out.println("                                                                      |__/                               |__/");
     }
 }
 
