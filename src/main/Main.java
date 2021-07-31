b8eb54bb2659f947cdebb0e566170347b4444945
jim-low <robil12380@gmail.com>
2021-07-31 10:09:27 +0800

    fix: remove comment and method call

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,9 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        showAcademyBanner();
     }
 
     public static void showAcademyBanner() {
-        // whoever messes up the formatting and/or alignment of this here banner, will, by default, accept the consequences to come
         System.out.println("__      __   _                    _         ___       __ _      _ _            _              _");
         System.out.println("\\ \\    / /__| |__ ___ _ __  ___  | |_ ___  |_ _|_ _  / _(_)_ _ (_) |_ _  _    /_\\  __ __ _ __| |___ _ __ _  _");
         System.out.println(" \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_) |  _/ _ \\  | || ' \\|  _| | ' \\| |  _| || |  / _ \\/ _/ _` / _` / -_) '  \\ || |");
