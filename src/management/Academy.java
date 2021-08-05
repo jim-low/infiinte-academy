4ac15ffda419705df473c7e197eaf4a667ab0076
jim-low <robil12380@gmail.com>
2021-08-05 09:29:17 +0800

    chore(management): start on first time login user interface

--- src/management/Academy.java
+++ src/management/Academy.java
@@ -11,5 +11,15 @@ public class Academy {
         System.out.println("  \\_/\\_/\\___|_\\__\\___/_|_|_\\___|  \\__\\___/ |___|_||_|_| |_|_||_|_|\\__|\\_, | /_/ \\_\\__\\__,_\\__,_\\___|_|_|_\\_, |");
         System.out.println("                                                                      |__/                               |__/");
     }
+
+    public static void splashMenu() {
+        showAcademyBanner();
+    }
+
+    public static void firstLoginMenu() {
+        System.out.println("1. Sign Up For An Account In Infinity Academy");
+        System.out.println("2. About Infinity Academy");
+        System.out.println("3. Meet The Team");
+    }
 }
 
