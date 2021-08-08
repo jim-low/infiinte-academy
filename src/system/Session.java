583b7408519cae1abf64ef59a2419aad44f0e260
diongzy <yuii.yuii265@gmail.com>
2021-08-08 22:39:24 +0800

    feat(system): add basic class setup

--- src/system/Session.java
+++ src/system/Session.java
@@ -1,6 +1,43 @@
 package system;
 
+import personnel.*;
 public class Session {
     // type your code here, all the best
-}
+    private Slot slot;
+    private Course course;
+    private Instructor instructor;
 
+    public Session(Slot slot, Course course, Instructor instructor) {
+        this.slot = slot;
+        this.course = course;
+        this.instructor = instructor;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+    
+    public String toString(){
+        return String.format("%-10s %-10s %-10s\n", slot, course, instructor);
+    }
+}
