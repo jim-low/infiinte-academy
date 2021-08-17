7ebebc7392a4437289b0b39458d020dd2d9485be
weiSan02 <86460890+weiSan02@users.noreply.github.com>
2021-08-17 23:03:51 +0800

    fix(personnel): merge conflicts from main

--- src/personnel/Person.java
+++ src/personnel/Person.java
@@ -1,7 +1,5 @@
 package personnel;
 
-import java.util.Scanner;
-
 public abstract class Person {
     // type your code here, all the best
 
@@ -13,6 +11,10 @@ public abstract class Person {
 
     public Person(){
         this("","","","");
+    }
+    
+    public Person(Person person){
+        this(person.name, person.icNo, person.gender, person.email);
     }
     
     public Person(String name) {
@@ -31,5 +33,37 @@ public abstract class Person {
         this.email = email;
         this.paidFee = false;
     }  
-}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public boolean hasPaidFee(){
+        return false;
+    }
+
+    public void setPaidFee(boolean paidFee) {
+        this.paidFee = paidFee;
+    }
+    
+    public static <T> T search (String name, Class<T> type){
+        T person = null;
+        
+        if(type.equals(Student.class)){
+            person = type.cast(Student.search(name));
+        } else if(type.equals(Instructor.class)){
+            person = type.cast(Instructor.search(name));
+        }    
+        
+        return person;
+    }
+ }
 
