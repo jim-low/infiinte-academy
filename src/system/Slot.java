b7cf9feaa7c65085c58fe06b5038e3bd31ab46ad
diongzy <yuii.yuii265@gmail.com>
2021-08-22 16:19:44 +0800

    feat: add method to list slots, add method to setup time slot, add pre-made time slots

--- src/system/Slot.java
+++ src/system/Slot.java
@@ -6,10 +6,48 @@ public class Slot {
     // type your code here, all the best
     Calendar startTime;
     Calendar endTime;
-
+    public final static Slot[] AVAILABLE_SLOTS = {
+        new Slot(setupTimeSlot(9, 00), setupTimeSlot(10, 30)),
+        new Slot(setupTimeSlot(10, 00), setupTimeSlot(10, 00)),
+        new Slot(setupTimeSlot(9, 00), setupTimeSlot(11, 00)),
+        new Slot(setupTimeSlot(10, 00), setupTimeSlot(12, 00)),
+        new Slot(setupTimeSlot(10, 00), setupTimeSlot(11, 00)),
+        new Slot(setupTimeSlot(14, 00), setupTimeSlot(16, 00)),
+        new Slot(setupTimeSlot(16, 30), setupTimeSlot(18, 30)),
+        new Slot(setupTimeSlot(16, 00), setupTimeSlot(17, 30)),
+        new Slot(setupTimeSlot(16, 00), setupTimeSlot(17, 00))
+    };
+    
     public Slot(Calendar startTime, Calendar endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+    
+    private static Calendar setupTimeSlot(int hours, int minutes){
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR, hours);
+        c.set(Calendar.MINUTE, minutes);
+        return c;
+    }
+    
+    public static void listSlots(){
+        System.out.println("\tStart Time\tEnd Time\n");
+        for (int i = 0; i < AVAILABLE_SLOTS.length; i++) {
+            Slot slot = AVAILABLE_SLOTS[i];
+            System.out.printf("%d.\t%s\n", i+1, slot.toString());
+        }
+    }
+    
+    public static Slot search(int index){
+        if(index < 0 || index >= AVAILABLE_SLOTS.length){
+            return null;
+        }
+        
+        return AVAILABLE_SLOTS[index];
+    }
+    
+    public String toString(){
+        return String.format("%s\t%s\n", this.getStartTime(), this.getEndTime());
     }
 
     public String getStartTime() {
@@ -28,9 +66,5 @@ public class Slot {
 
     public void setEndTime(Calendar endTime) {
         this.endTime = endTime;
-    }
-    
-    public String toString(){
-        return String.format("%s\t%s", this.getStartTime(), this.getEndTime());
     }
 }
